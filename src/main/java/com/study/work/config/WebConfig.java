91b0d79aea330a69782c8765dec59e197fcbed88
장현빈 <gusqls904@naver.com>
2025-09-10 14:14:28 +0900

    라우팅 설정 추가

---
+++ src/main/java/com/study/work/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.study.work.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // SPA 라우팅을 위한 설정
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/index.html");
+    }
+}
