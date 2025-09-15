afb4200f80fd883a757071e3f5a601ad617503dc
장현빈 <gusqls904@naver.com>
2025-09-15 10:27:05 +0900

    형상관리 변경 backend / frondend 분기 처리 및 빌드 변경

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -22,6 +22,8 @@ export default defineConfig({
     }
   },
   build: {
+    // 빌드 결과물을 backend/src/main/resources/static 폴더에 출력
+    outDir: '../backend/src/main/resources/static',
     // 빌드 시 public 폴더의 구조 유지
     copyPublicDir: true,
     // assets 폴더명 변경 (선택사항)
